5ac2668a6654a6a265ffc6b9a1c67d8aa020c86b
lajczi <lajczi@gmail.com>
2026-06-25 18:35:46 +0200

    Fixed warning after server startup

--- plugin/src/main/java/xyz/lychee/lagfixer/LagFixerLoader.java
+++ plugin/src/main/java/xyz/lychee/lagfixer/LagFixerLoader.java
@@ -14,21 +14,15 @@ public class LagFixerLoader implements PluginLoader {
         MavenLibraryResolver mavenResolver = new MavenLibraryResolver();
 
         mavenResolver.addRepository(
-                new RemoteRepository.Builder("central", "default", "https://repo.maven.apache.org/maven2/").build()
-        );
-
-        mavenResolver.addRepository(
                 new RemoteRepository.Builder("paper", "default", "https://repo.papermc.io/repository/maven-public/").build()
         );
 
         mavenResolver.addDependency(
                 new Dependency(new DefaultArtifact("org.apache.commons:commons-lang3:3.20.0"), null)
         );
-
         mavenResolver.addDependency(
                 new Dependency(new DefaultArtifact("commons-io:commons-io:2.22.0"), null)
         );
-
         mavenResolver.addDependency(
                 new Dependency(new DefaultArtifact("com.github.oshi:oshi-core:7.3.1"), null)
         );
